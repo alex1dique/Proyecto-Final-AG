d35d6e4bdd09add6da0d56e7c851bbbf3e7689e9
bitha121295 <abyalax18@gmail.com>
2016-07-03 15:33:12 -0600

    modificaciones y subida de imagenes

--- Invernadero/Invernadero.ino
+++ Invernadero/Invernadero.ino
@@ -1,15 +1,19 @@
-#include <Ultrasonic.h>
+//#include <Ultrasonic.h>
 
-#include <DHT.h>
+//#include <DHT.h>
 
 //VARIABLES DE PRUEVA
 int estado = LOW;
 //-----------------------
 
+//declarando variables LDR
+  int sensorPin = A0; // select the input pin for ldr
+  int sensorValue = 0; // variable to store the value coming from the sensor
+  
 //declarando variables ultrasonido
  #define TRIGGER_PIN  3
  #define ECHO_PIN     2
- Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+ //Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
  
  //declarando variables relay shield
  int foco = 5;    // Digital Arduino Pin para encender el foco
@@ -22,16 +26,16 @@ int estado = LOW;
  int temp;//temperatura 
  int humedad;//humedad 
  
- DHT dht (SENSOR, DHT22);
+// DHT dht (SENSOR, DHT22);
  
  //declarando variables PIR
  int LEDPin= 13;
  int PIRPin= 9;
- 
+
 // the setup routine runs once when you press reset:
 void setup()  {
     
-    dht.begin();
+//    dht.begin();
     Serial.begin(9600);
     
     //declarar pid 4 al 6 como  output:
@@ -50,51 +54,10 @@ void loop()  {
    ultrasonido();
    Serial.print(""); 
    dth22(); 
+   Serial.print(""); 
+   LDR();  
    Serial.print("");
-   pir();   
-}
-
-
-void luz()
-{
-   //foco
-    
-    digitalWrite(foco,HIGH);// NO3 and COM3 Connected 
-    delay(10000); // wait 10000 milliseconds (1 second)
-    digitalWrite(foco,LOW);// NO3 and COM3 Disconnected 
-    delay(10000); // wait 10000 milliseconds (1 second)
-    
-}
-
-void vent()
-{
-
-     //ventilador
-    
-    digitalWrite(ventilador,HIGH);// NO4 and COM4 Connected 
-    delay(10000); // wait 10000 milliseconds (1 second)
-    digitalWrite(ventilador,LOW);// NO4 and COM4 Disconnected 
-    delay(10000); // wait 10000 milliseconds (1 second)
-    
-}
-
-void seca()
-{
-
-     //secadora
-    
-    digitalWrite(secadora,HIGH);// NO1 and COM1 Connected 
-    delay(10000); // wait 10000 milliseconds (1 second)
-    digitalWrite(secadora,LOW);// NO1 and COM1 Disconnected 
-    delay(10000); // wait 10000 milliseconds (1 second)
-    
-}
-
-void bomba()
-{
-  
-      //bombita
-
+   pir(); 
 }
 
 void dth22()
@@ -102,17 +65,25 @@ void dth22()
     //sensor de temperatura y humedad
     humedad = dht.readHumidity();
     temp = dht.readTemperature();
-    if(temp)
-    /*lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Temperatura:");
-    lcd.print(temp,DEC);
-    lcd.print("C");
-    lcd.setCursor(0,1);
-    lcd.print("Humedad:");
-    lcd.print(humedad,DEC);
-    lcd.print("%");*/
-    //
+    if(temp>30){
+      digitalWrite(ventilador,HIGH);
+      }
+    if else(temp<15){
+      digitalWrite(secadora,HIGH);
+      
+      }  
+    if else(humedad<60){
+      digitalWrite(bombita,HIGH);
+      } 
+      if else(humedad>70){
+      digitalWrite(secadora,HIGH);
+      
+      }     
+    else {
+      digitalWrite(ventilador,LOW);
+      digitalWrite(secadora,LOW);
+      }  
+    //imprimiendo temperatura
     Serial.print("Temperatura:");
     Serial.print(temp,DEC);
     Serial.println("C");
@@ -131,11 +102,13 @@ void ultrasonido()
 
   cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
 
-  if(cmMsec>=15.00){
-    digitalWrite(LEDPin, LOW); 
+  if(cmMsec>=25.00){
+    digitalWrite(LEDPin, HIGH);
+    
+    digitalWrite(bombita, LOW);
     }
   else{
-    digitalWrite(LEDPin, HIGH); 
+    digitalWrite(LEDPin, LOW);  
     }
 
   Serial.print("CM: ");
@@ -144,23 +117,34 @@ void ultrasonido()
   
   delay(1000);
 
-  }
+  }   
   void pir()
 {
   int value= digitalRead(PIRPin);
  
   if (value == HIGH)
   {
-    Serial.print("Alerta humano detectado")
+    Serial.println("Alerta humano detectado");
     digitalWrite(LEDPin, HIGH);
-    delay(50);
+    delay(100);
     digitalWrite(LEDPin, LOW);
-    delay(50);
+    delay(100);
   }
   else
   {
     digitalWrite(LEDPin, LOW);
   }
+}
+void LDR(){
+sensorValue = analogRead(sensorPin); // read the value from the sensor
+if (sensorValue>=980){
+  digitalWrite(foco, HIGH);
+  }
+else{
+  digitalWrite(foco, LOW);
+  }  
+Serial.println(sensorValue); //prints the values coming from the sensor on the screen
+delay(100);
 }
 
  
